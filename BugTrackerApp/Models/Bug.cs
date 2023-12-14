95bc43cae7370b52f09b23d660db32ea93c7ebd1
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2023-12-14 11:20:11 +0530

    Add files via upload

---
+++ BugTrackerApp/Models/Bug.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BugTrackerApp.Models
+{
+    public class Bug
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public string BugName { get; set; }
+        public bool IsCompleted { get; set; }
+        public string Notes { get; set; }
+    }
+
+}
