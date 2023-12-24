1531deb6f38504b08146be88a7fa8f56fca1d4c0
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2023-12-24 10:22:02 +0530

    Add files via upload

---
+++ BugTrackerApp/Controllers/BugTrackerController.cs
@@ -0,0 +1,76 @@
+﻿using BugTrackerApp.DAL.Interface;
+using BugTrackerApp.DAL.Repository;
+using BugTrackerApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BugTrackerApp.Controllers
+{
+    public class BugTrackerController : Controller
+    {
+        private readonly IBugTrackerInterface _Repository;
+        public BugTrackerController(IBugTrackerInterface service)
+        {
+            _Repository = service;
+        }
+        public BugTrackerController()
+        {
+            // Constructor logic, if needed
+        }
+        // GET: BugTracker
+        public ActionResult Index()
+        {
+            //write your code here
+            throw new NotImplementedException(); 
+        }
+
+        public ViewResult Details(int id)
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+
+        public ActionResult Create()
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Bug Bug)
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+
+        public ActionResult EditAsync(int id)
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+        [HttpPost]
+        public ActionResult Edit(Bug Bug)
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+
+        public ActionResult Delete(int id, bool? saveChangesError)
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+    }
+}
