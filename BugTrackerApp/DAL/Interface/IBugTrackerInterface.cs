bdf17a0e402cfa230dfbad99175a287105c469fd
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2024-01-03 15:15:05 +0530

    Add files via upload

---
+++ BugTrackerApp/DAL/Interface/IBugTrackerInterface.cs
@@ -0,0 +1,19 @@
+﻿using BugTrackerApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BugTrackerApp.DAL.Interface
+{
+    public interface IBugTrackerInterface 
+    {
+        IEnumerable<Bug> GetBugs();
+        Bug GetBugByID(int BugId);
+        Bug InsertBug(Bug Bug);
+        int DeleteBug(int BugId);
+        bool UpdateBug(Bug Bug);
+        void Save();
+    }
+}
